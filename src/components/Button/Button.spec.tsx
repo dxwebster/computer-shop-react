82eab672a9a6307a2aaba962392a3fe783f52b8f
dxwebster <adrianalimafm@gmail.com>
2022-02-18 22:53:08 -0300

    fix: fix responsability of button component

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -1,4 +1,4 @@
-import { render, waitFor, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import Button from '.';
 
